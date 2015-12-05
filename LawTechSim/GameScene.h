b8837a5ea6891685d0850f1457253c9ba0654a52
Leo Shimonaka <lshimonaka@gmail.com>
2015-12-05 17:35:44 -0500

    Initial Commit

---
+++ LawTechSim/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  LawTechSim
+//
+
+//  Copyright (c) 2015 Leo Shimonaka. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ LawTechSim/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  LawTechSim
+//
+
+//  Copyright (c) 2015 Leo Shimonaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
